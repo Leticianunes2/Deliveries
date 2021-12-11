5397075b5698c32e319b05cc1428f438f0e3ed16
Leticianunes2 <leticianunes29021992@gmail.com>
2021-12-11 20:54:32 -0300

    primeiro commit

--- app.js
+++ app.js
@@ -1,9 +1,5 @@
-
-
 const express = require("express")
-
 const app = express()
-
 
 app.use(express.json())
 
@@ -11,10 +7,8 @@ app.listen(5432,() =>{
     console.log(`servidor rodando` + 5432)
 })
 
-
 const index = require("./routes/index")
 const deliveries = require("./routes/deliveriesRoutes")
-
 
 app.use("/", index)
 app.use("/deliveries", deliveries)
